31cb1ceea2ed24a45da076447fadf79b8293d9df
peppedilillo <peppedilillo@gmail.com>
2024-09-26 00:14:12 +0200

    added posts to admin. a couple hello world views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from . import models
 
 
-def index(request):
-    return HttpResponse("Hello, world.")
+def index(request: HttpResponse) -> HttpResponse:
+    latest_posts = models.Post.objects.order_by("-date")[:5]
+    latest_posts_titles = [p.title for p in latest_posts]
+    return HttpResponse(", ".join(latest_posts_titles))
+
+
+def detail(request: HttpResponse, post_id: int) -> HttpResponse:
+    post = get_object_or_404(models.Post, pk=post_id)
+    return HttpResponse(f"{post.title} {post.url} ({post.date})")
