3d03209aa359eb6d1ee38fbddd134128b488ce85
peppedilillo <peppedilillo@gmail.com>
2024-10-05 18:47:07 +0200

    improved login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect, reverse
+from django.contrib.auth import login
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
 
 
 def signup(request: HttpResponse) -> HttpResponse:
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.groups.add(Group.objects.get(name="user"))
+            login(request, user)
             return HttpResponseRedirect(reverse("app:index"))
     else:
         form = UserCreationForm()
-    return render(request, "registration/login.html", {"form": form})
+    return render(request, "registration/signup.html", {"form": form})
