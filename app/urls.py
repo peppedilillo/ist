ff6f055d2a50fbf69369bdb27db0f7a270a8a3e9
peppedilillo <peppedilillo@gmail.com>
2024-10-14 22:26:34 +0200

    minors

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,12 @@ app_name = "app"
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
+    path("posts/submit/", views.post_submit, name="post_submit"),
+
     path("posts/<int:post_id>/comment", views.post_comment, name="post_comment"),
     path("posts/<int:post_id>/delete", views.post_delete, name="post_delete"),
     path("posts/<int:post_id>/edit", views.post_edit, name="post_edit"),
     path("comments/<int:comment_id>/reply", views.comment_reply, name="comment_reply"),
     path("comments/<int:comment_id>/delete", views.comment_delete, name="comment_delete"),
     path("comments/<int:comment_id>/edit", views.comment_edit, name="comment_edit"),
-    path("posts/submit/", views.post_submit, name="post_submit"),
 ]
