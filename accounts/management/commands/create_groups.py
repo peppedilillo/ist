d281bc2d38e9b1daac331e41490b3f53c74b8e6f
peppedilillo <peppedilillo@gmail.com>
2024-10-21 23:45:32 +0200

    adding management commands to create groups

---
+++ accounts/management/commands/create_groups.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
+
+GROUPS_PERMISSIONS = {
+    "moderator": ("change_post", "delete_post", "change_comment", "delete_comment"),
+}
+
+
+class Command(BaseCommand):
+    help = 'Creates groups and assigns permissions'
+
+    def handle(self, *args, **options):
+        for group, permissions in GROUPS_PERMISSIONS.items():
+            group, created = Group.objects.get_or_create(name='moderator')
+            if not created:
+                continue
+            for permission in permissions:
+                p = Permission.objects.get(codename=permission)
+                group.permissions.add(p)
--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 docker-compose up -d
+echo "[1/3] Running migrations"
 python manage.py makemigrations accounts
 python manage.py makemigrations app
 python manage.py makemigrations demo
 python manage.py migrate
+echo "[2/3] Crating superuser.."
 python manage.py createsuperuser
+echo "[3/3] Creating user groups"
+python manage.py create_groups
