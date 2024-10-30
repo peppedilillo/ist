93ad3022609edd7d8c2db74fd94c6a9e557b721c
peppedilillo <peppedilillo@gmail.com>
2024-10-30 20:47:49 +0100

    scores needs to be updated at seconds intervals

--- app/scores.py
+++ app/scores.py
@@ -6,4 +6,4 @@ arbitrary_date = datetime.fromisoformat("2024-01-01T00:00:00Z")
 
 def compute_score(nlikes: int, creation_date, days_modifier: int = 1):
     lapse = creation_date - arbitrary_date
-    return log10(max(nlikes, 1)) + (lapse.days / days_modifier)
+    return log10(max(nlikes, 1)) + (lapse.seconds / (days_modifier * 24 * 60 * 60))
