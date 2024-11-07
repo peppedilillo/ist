3080f049e1679adc9d22c360a3fcef7cd2ae39c2
peppedilillo <peppedilillo@gmail.com>
2024-11-07 22:40:09 +0100

    fixed score

--- mb/scores.py
+++ mb/scores.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from math import log10
 
 arbitrary_date = datetime.fromisoformat("2024-01-01T00:00:00Z")
+day_seconds = 24 * 60 * 60
 
 
 def compute_score(nlikes: int, creation_date, days_modifier: int = 1):
     lapse = creation_date - arbitrary_date
-    return log10(max(nlikes, 1)) + (lapse.seconds / (days_modifier * 24 * 60 * 60))
+    return log10(max(nlikes, 1)) + (lapse.days + lapse.seconds / day_seconds) / days_modifier
